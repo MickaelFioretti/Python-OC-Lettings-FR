9825db87ea8946276dd2c0159a6d3b196ed9f696
Mickael Fioretti <mickael.fioretti@drotek.com>
2024-07-15 15:40:41 +0200

    feat: Add docstring for main app

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
+    """
+    Vue pour la page d'accueil.
+
+    Cette vue affiche la page d'accueil du site.
+
+    Args:
+        request (HttpRequest): L'objet de requête HTTP.
+
+    Returns:
+        HttpResponse: La réponse HTTP contenant le rendu de la page d'accueil.
+    """
     return render(request, "index.html")
