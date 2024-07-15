c072ec185289faa5387af8ab0c96e9ee4095e158
Mickael Fioretti <mickael.fioretti@drotek.com>
2024-07-15 15:40:09 +0200

    refactor: Add docstrings to Profile model and views

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Représente le profil utilisateur associé à un compte utilisateur.
+
+    Attributs:
+        user (User): Une relation OneToOne avec le modèle User de Django.
+
+        favorite_city (str): La ville préférée de l'utilisateur, peut être laissée vide.
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
+        """
+        Retourne une représentation sous forme de chaîne de caractères du profil.
+
+        Returns:
+            str: Le nom d'utilisateur associé à ce profil.
+        """
         return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,35 @@ from .models import Profile
 
 # Create your views here.
 def profiles_index(request):
+    """
+    Vue pour l'index des profils.
+
+    Cette vue affiche une liste de tous les profils utilisateurs disponibles.
+
+    Args:
+        request (HttpRequest): L'objet de requête HTTP.
+
+    Returns:
+        HttpResponse: La réponse HTTP contenant le rendu de la page des profils.
+    """
     profiles_list = Profile.objects.all()
     context = {"profiles_list": profiles_list}
     return render(request, "profiles_index.html", context)
 
 
 def profile(request, username):
+    """
+    Vue pour un profil utilisateur spécifique.
+
+    Cette vue affiche les détails d'un profil utilisateur basé sur le nom d'utilisateur fourni.
+
+    Args:
+        request (HttpRequest): L'objet de requête HTTP.
+        username (str): Le nom d'utilisateur du profil à afficher.
+
+    Returns:
+        HttpResponse: La réponse HTTP contenant le rendu de la page du profil.
+    """
     profile = Profile.objects.get(user__username=username)
     context = {"profile": profile}
     return render(request, "profile.html", context)
