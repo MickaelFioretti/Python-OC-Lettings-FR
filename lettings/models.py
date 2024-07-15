3c7f17ac391572e3f9ca35b0bc0d01a9e5d06fbf
Mickael Fioretti <mickael.fioretti@drotek.com>
2024-07-15 15:40:00 +0200

    feat: docstring for app lettings

--- lettings/models.py
+++ lettings/models.py
@@ -3,6 +3,19 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    """
+    Représente une adresse physique.
+
+    Attributs:
+
+        number (int): Le numéro de la rue, limité à 9999.
+        street (str): Le nom de la rue, avec un maximum de 64 caractères.
+        city (str): Le nom de la ville, avec un maximum de 64 caractères.
+        state (str): L'état ou la région, exactement 2 caractères.
+        zip_code (int): Le code postal, limité à 99999.
+        country_iso_code (str): Le code ISO du pays, exactement 3 caractères.
+    """
+
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -13,12 +26,33 @@ class Address(models.Model):
     )
 
     def __str__(self):
+        """
+        Retourne une représentation sous forme de chaîne de caractères de l'adresse.
+
+        Returns:
+            str: L'adresse formatée avec le numéro et le nom de la rue.
+        """
         return f"{self.number} {self.street}"
 
 
 class Letting(models.Model):
+    """
+    Représente une location avec un titre et une adresse associée.
+
+    Attributs:
+
+        title (str): Le titre de la location, avec un maximum de 256 caractères.
+        address (Address): Une relation OneToOne avec le modèle Address.
+    """
+
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
+        """
+        Retourne une représentation sous forme de chaîne de caractères de la location.
+
+        Returns:
+            str: Le titre de la location.
+        """
         return self.title
--- lettings/views.py
+++ lettings/views.py
@@ -3,12 +3,35 @@ from .models import Letting
 
 
 def lettings_index(request):
+    """
+    Vue pour l'index des locations.
+
+    Cette vue affiche une liste de toutes les locations disponibles.
+
+    Args:
+        request (HttpRequest): L'objet de requête HTTP.
+
+    Returns:
+        HttpResponse: La réponse HTTP contenant le rendu de la page des locations.
+    """
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
     return render(request, "lettings_index.html", context)
 
 
 def letting(request, letting_id):
+    """
+    Vue pour une location spécifique.
+
+    Cette vue affiche les détails d'une location basée sur l'ID de la location fourni.
+
+    Args:
+        request (HttpRequest): L'objet de requête HTTP.
+        letting_id (int): L'ID de la location à afficher.
+
+    Returns:
+        HttpResponse: La réponse HTTP contenant le rendu de la page de la location.
+    """
     letting = Letting.objects.get(id=letting_id)
     context = {
         "title": letting.title,
